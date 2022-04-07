287930bb4d1d5fef7cf4e358028ada1b07829e13
SRvanBeek <0705stijnvb@gmail.com>
2022-04-07 14:11:55 +0200

    bomb math tiles

--- src/main/java/Bomb.java
+++ src/main/java/Bomb.java
@@ -2,6 +2,7 @@ import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.component.Component;
 import com.almasb.fxgl.entity.components.BoundingBoxComponent;
+import com.almasb.fxgl.physics.PhysicsComponent;
 import javafx.util.Duration;
 
 import static com.almasb.fxgl.dsl.FXGL.getGameTimer;
@@ -9,6 +10,7 @@ import static com.almasb.fxgl.dsl.FXGL.getGameWorld;
 
 public class Bomb extends Component {
     private int radius;
+    private PhysicsComponent physics;
 
     public Bomb(int radius) {
         this.radius = radius;
@@ -20,6 +22,10 @@ public class Bomb extends Component {
                 bomb.removeFromWorld();
             }
         }, Duration.seconds(4));
+    }
+
+    public PhysicsComponent getPhysics() {
+        return physics;
     }
 }
 
